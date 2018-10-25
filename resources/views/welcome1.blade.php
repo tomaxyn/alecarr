e28f105bdd531d408ec771d03b74d88d4d645d73
tomaxyn <ackwebroad@gmail.com>
2018-10-25 10:43:48 -0500

    inicio

---
+++ resources/views/welcome1.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Alejandra Carrillo</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                color: hotpink;
+                font-size: 144px;
+                font-weight: 900;
+            }
+            .litle{
+                color: #2465b5;
+                font-size: 108px;
+                font-weight: 800;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Ale Carrillo<br>
+                    <span class="litle">Cuenta Conmigo</span>
+                </div>
+                
+            </div>
+        </div>
+    </body>
+</html>
+
